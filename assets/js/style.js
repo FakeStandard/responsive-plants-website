89cb89799c3681b5147421866459f97ee0ade331
riley.lin <rileylin.space@gmail.com>
2023-10-08 12:27:26 +0800

    change background header

--- assets/js/style.js
+++ assets/js/style.js
@@ -24,3 +24,12 @@ function linkAction() {
 }
 
 navLink.forEach(f => f.addEventListener('click', linkAction))
+
+// CHANGE BACKGROUND HEADER
+function scrollHeader() {
+    const header = document.getElementById('header')
+    if (this.scrollY >= 80) header.classList.add('scroll-header')
+    else header.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
