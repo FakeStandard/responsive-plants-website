b3fe13011d33fbbb1b12258109cf587092ba8ae5
riley.lin <rileylin.space@gmail.com>
2023-10-08 15:59:45 +0800

    add scroll up button

--- assets/js/style.js
+++ assets/js/style.js
@@ -84,3 +84,13 @@ function scrollActive() {
 }
 
 window.addEventListener('scroll', scrollActive)
+
+// SHOW SCROLL UP
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up')
+
+    if (this.scrollY >= 200) scrollUp.classList.add('show-scroll')
+    else scrollUp.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll', scrollUp)
