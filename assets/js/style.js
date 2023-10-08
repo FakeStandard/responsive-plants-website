3783f0cc1d8231594ebc7f1bc88d71e05ce98986
riley.lin <rileylin.space@gmail.com>
2023-10-08 13:21:38 +0800

    add FAQs

--- assets/js/style.js
+++ assets/js/style.js
@@ -33,3 +33,33 @@ function scrollHeader() {
 }
 
 window.addEventListener('scroll', scrollHeader)
+
+// QUESTIONS ACCORDION
+const accordionItems = document.querySelectorAll('.questions-item')
+
+accordionItems.forEach((item) => {
+    const accordionHeader = item.querySelector('.questions-header')
+
+    accordionHeader.addEventListener('click', () => {
+        const openItem = document.querySelector('.accordion-open')
+
+        toggleItem(item)
+
+        if (openItem && openItem !== item) {
+            toggleItem(openItem)
+        }
+    })
+})
+
+const toggleItem = (item) => {
+    const accordionContent = item.querySelector('.questions-content')
+
+    if (item.classList.contains('accordion-open')) {
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    } else {
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+    }
+
+}
